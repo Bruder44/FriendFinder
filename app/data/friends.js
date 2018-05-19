8c9c65c733763ab8135abee1f82352f99bb7c811
Matt Bruder <mbruder@students.kennesaw.edu>
2018-05-19 02:47:06 -0400

    last push before deployment

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+      name:"David DeGea",
+      photo:"https://i2.wp.com/celebrityunfold.com/wp-content/uploads/2017/08/David-de-Gea-manchester-united.jpg?resize=245%2C300",
+      scores:[
+        "5",
+        "1",
+        "4",
+        "2",
+        "4",
+        "1",
+        "5",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Jamie Vardy",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Leicester_1_Chelsea_2_%2836325510703%29_%28cropped%29.jpg/200px-Leicester_1_Chelsea_2_%2836325510703%29_%28cropped%29.jpg",
+      scores: [
+        "3",
+        "4",
+        "3",
+        "4",
+        "3",
+        "1",
+        "5",
+        "1",
+        "4",
+        "4"
+      ]
+    },
+    {
+      name: "Dele Alli",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Dele_Alli_2018_%28cropped%29.jpg/1200px-Dele_Alli_2018_%28cropped%29.jpg",
+      scores: [
+        "4",
+        "5",
+        "3",
+        "4",
+        "5",
+        "3",
+        "4",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Zlatan Ibrahimovic",
+      photo: "https://cdn.images.express.co.uk/img/dynamic/67/590x/Zlatan-Ibrahimovic-Manchester-United-936594.jpg",
+      scores: [
+        "5",
+        "1",
+        "5",
+        "5",
+        "4",
+        "5",
+        "4",
+        "3",
+        "5",
+        "3"
+      ]
+    },
+    {
+      name: "Wayne Rooney",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Wayne_Rooney_144855cropped.jpg/300px-Wayne_Rooney_144855cropped.jpg",
+      scores: [
+        "3",
+        "2",
+        "3",
+        "4",
+        "1",
+        "3",
+        "4",
+        "3",
+        "5",
+        "1"
+      ]
+    },
+    {
+      name: "Paul Pogba",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Paul_Pogba_2018.jpg/300px-Paul_Pogba_2018.jpg",
+      scores: [
+        "5",
+        "5",
+        "1",
+        "3",
+        "1",
+        "5",
+        "4",
+        "3",
+        "5",
+        "5"
+      ]
+    }
+  ]
+
+// Export the friends array. This makes it accessible to the other files using require()
+module.exports = friends;
